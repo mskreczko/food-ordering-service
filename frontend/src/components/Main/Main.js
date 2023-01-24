8986be52cc6104b50ffb6fed7aa0010212894192
mskreczko <mic.skreczko@gmail.com>
2023-01-24 18:35:02 +0100

    changed image hosting to self-hosting

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -5,7 +5,7 @@ export default function Main() {
     return (
         <main>
             <div>
-                <a href="https://www.freepnglogos.com/pics/food" title="Image from freepnglogos.com"><img className='hero-img' src="https://www.freepnglogos.com/uploads/food-png/download-food-png-file-png-image-pngimg-1.png" alt="download food png file png image pngimg" /></a>
+                <a href="https://www.freepnglogos.com/pics/food" title="Image from freepnglogos.com"><img className='hero-img' src="/food-13528.png" alt="download food png file png image pngimg" /></a>
             </div>
             <div className='hero-text'>
                 <h2 id='hero-slogan'>The food at your doorstep</h2>
