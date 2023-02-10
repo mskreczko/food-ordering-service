027872c0cf6c3b677990fa41ad6d9aa94434c0aa
mskreczko <mic.skreczko@gmail.com>
2023-02-10 19:29:43 +0100

    implemented endpoint for fetching current orders for admin

--- api/foodordering/src/main/java/pl/mskreczko/foodordering/admin/AdminOrderController.java
+++ api/foodordering/src/main/java/pl/mskreczko/foodordering/admin/AdminOrderController.java
@@ -2,9 +2,13 @@ package pl.mskreczko.foodordering.admin;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.mskreczko.foodordering.order.Order;
 import pl.mskreczko.foodordering.order.OrderService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,7 +17,8 @@ public class AdminOrderController {
 
     private final OrderService orderService;
 
-    public ResponseEntity<?> getAllOrders() {
-        return ResponseEntity.ok("Hello");
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders() {
+        return ResponseEntity.ok(orderService.getAllOrders());
     }
 }
--- api/foodordering/src/main/java/pl/mskreczko/foodordering/order/OrderService.java
+++ api/foodordering/src/main/java/pl/mskreczko/foodordering/order/OrderService.java
@@ -50,4 +50,8 @@ public class OrderService {
         order.setCustomer(user);
         orderRepository.save(order);
     }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
 }
